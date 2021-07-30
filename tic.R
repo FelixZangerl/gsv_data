a1802b827722a76e7a629bdf236ab22bb36b3559
ZangerlF <felixz@disroot.org>
2021-07-30 10:26:37 +0200

    solve problem with http2

--- tic.R
+++ tic.R
@@ -6,6 +6,8 @@ get_stage("deploy") %>%
   add_step(step_install_github("trendecon/trendecon")) %>%
   add_step(step_install_cran("prophet")) %>%
   add_step(step_install_cran("tibble")) %>%
+  add_step(step_install_cran("httr")) %>%
+  add_code_step(withr::with_package("httr", set_config(config(http_version = 0)))) %>%
   add_code_step(library(prophet)) %>%
   add_code_step(withr::with_package("trendecon", proc_trendecon_at())) %>%
   add_code_step(withr::with_package("trendecon", proc_index(c("mango","zara","H&M","blue tomato","schuhe kaufen", "deichmann"), "AT", "clothing"))) %>%
