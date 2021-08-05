ea138f8ad9bb831f1929336d31aed25e48549c29
ZangerlF <felixz@disroot.org>
2021-08-05 16:02:35 +0200

    update data

--- tic.R
+++ tic.R
@@ -8,23 +8,23 @@ get_stage("deploy") %>%
   add_step(step_install_cran("tibble")) %>%
   add_step(step_install_cran("httr")) %>%
   add_code_step(withr::with_package("httr", set_config(config(http_version = 0)))) %>%
-  add_code_step(library(prophet)) %>%
+  add_code_step(library(prophet)) #%>%
 #  add_code_step(withr::with_package("trendecon", proc_trendecon_at())) %>%
-  add_code_step(geo <- "AT") %>%
-  add_code_step(withr::with_package("trendecon", proc_index(c("Wirtschaftskrise","Kurzarbeit","arbeitslos","Insolvenz"), geo, "economic_sentiment"))) %>%
-  add_code_step(withr::with_package("trendecon", proc_index(c("mango","zara","H&M","blue tomato","schuhe kaufen", "deichmann"), geo, "clothing"))) %>%
-  add_code_step(food_delivery <- c("take away", "takeaway", "pizza bestellen")) %>%
-  add_code_step(withr::with_package("trendecon", proc_index(food_delivery, geo, "food_delivery"))) %>%
-  add_code_step(home_office <- c("headset", "monitor","maus","hdmi")) %>%
-  add_code_step(withr::with_package("trendecon", proc_index(home_office, geo, "home_office"))) %>%
-  add_code_step(gardening <- c("Heim+Hobby","Bau+Hobby","Bauhaus","hornbach","obi")) %>%
-  add_code_step(withr::with_package("trendecon", proc_index(gardening, geo, "gardening"))) %>%
-  add_code_step(cultural <- c("kino","theater","cinema","cineplexx","oper","konzert","oeticket")) %>%
-  add_code_step(withr::with_package("trendecon", proc_index(cultural, geo, "cultural"))) %>%
-  add_code_step(travel <- c("städtetrip","flug buchen","günstige flüge")) %>%
-  add_code_step(withr::with_package("trendecon", proc_index(travel, geo, "travel"))) %>%
-  add_code_step(mobility <- c("Fahrplan","taxi","sixt","google maps")) %>%
-  add_code_step(withr::with_package("trendecon", proc_index(mobility, geo, "mobility"))) %>%
-  add_code_step(luxury <- c("juwelier","swarovski","uhr","uhren","christ","feichtinger")) %>%
-  add_code_step(withr::with_package("trendecon", proc_index(luxury, geo, "luxury"))) %>%
-  add_step(step_do_push_deploy())
+#  add_code_step(geo <- "AT") %>%
+#  add_code_step(withr::with_package("trendecon", proc_index(c("Wirtschaftskrise","Kurzarbeit","arbeitslos","Insolvenz"), geo, "economic_sentiment"))) %>%
+#  add_code_step(withr::with_package("trendecon", proc_index(c("mango","zara","H&M","blue tomato","schuhe kaufen", "deichmann"), geo, "clothing"))) %>%
+#  add_code_step(food_delivery <- c("take away", "takeaway", "pizza bestellen")) %>%
+#  add_code_step(withr::with_package("trendecon", proc_index(food_delivery, geo, "food_delivery"))) %>%
+#  add_code_step(home_office <- c("headset", "monitor","maus","hdmi")) %>%
+#  add_code_step(withr::with_package("trendecon", proc_index(home_office, geo, "home_office"))) %>%
+#  add_code_step(gardening <- c("Heim+Hobby","Bau+Hobby","Bauhaus","hornbach","obi")) %>%
+#  add_code_step(withr::with_package("trendecon", proc_index(gardening, geo, "gardening"))) %>%
+#  add_code_step(cultural <- c("kino","theater","cinema","cineplexx","oper","konzert","oeticket")) %>%
+#  add_code_step(withr::with_package("trendecon", proc_index(cultural, geo, "cultural"))) %>%
+#  add_code_step(travel <- c("städtetrip","flug buchen","günstige flüge")) %>%
+#  add_code_step(withr::with_package("trendecon", proc_index(travel, geo, "travel"))) %>%
+#  add_code_step(mobility <- c("Fahrplan","taxi","sixt","google maps")) %>%
+#  add_code_step(withr::with_package("trendecon", proc_index(mobility, geo, "mobility"))) %>%
+#  add_code_step(luxury <- c("juwelier","swarovski","uhr","uhren","christ","feichtinger")) %>%
+#  add_code_step(withr::with_package("trendecon", proc_index(luxury, geo, "luxury"))) %>%
+#  add_step(step_do_push_deploy())
--- trends.R
+++ trends.R
@@ -8,10 +8,10 @@ keywords <- c("Wirtschaftskrise","Kurzarbeit","arbeitslos","Insolvenz")
 #keywords <- "wirtschaftskrise"
 #keywords <- c("arbeitslos","insolvenz")
 
-#proc_keyword_init(keywords[1], geo)
-#proc_keyword_init(keywords[2], geo)
-#proc_keyword_init(keywords[3], geo)
-#proc_keyword_init(keywords[4], geo)
+proc_keyword_init(keywords[1], geo)
+proc_keyword_init(keywords[2], geo)
+proc_keyword_init(keywords[3], geo)
+proc_keyword_init(keywords[4], geo)
 
 proc_index(c("Wirtschaftskrise","Kurzarbeit","arbeitslos","Insolvenz"), 
            geo, "economic_sentiment")
@@ -110,3 +110,13 @@ proc_index(luxury, geo, "luxury")
 #ts_plot(x)
 #
 #ts_plot(economic_sentiment_sa)
+
+krise2 <- c("Krise", "Abschwung", "arbeitslos", "Rezession")
+
+proc_keyword_init(krise2[1], geo)
+proc_keyword_init(krise2[2], geo)
+proc_keyword_init(krise2[3], geo)
+proc_keyword_init(krise2[4], geo)
+
+proc_index(krise2, geo, "krise2")
+
