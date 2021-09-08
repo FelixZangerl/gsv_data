fea65cce6ffa2a947598c715c67b64824928aa18
ZangerlF <felixz@disroot.org>
2021-09-08 12:38:34 +0200

    monthly data init

---
+++ monthly_data.R
@@ -0,0 +1,13 @@
+#### MONTHLY DATA trendecon ######
+
+library(readr)
+library(dplyr)
+library(xts)
+library(tsbox)
+
+pes <- read_csv("./raw/at/economic_sentiment_sa.csv") %>% select(time, value) %>% ts_xts()
+
+pes_m <- apply.monthly(pes, mean)
+
+ts_dygraphs(pes_m)
+ts_dygraphs(pes)
