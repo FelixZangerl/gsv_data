7bd94a96761f8db9639b599f75eb86b957ee19f7
ZangerlF <felixz@disroot.org>
2022-01-04 12:38:46 +0100

    moment scrape

--- moment.R
+++ moment.R
@@ -1,7 +1,9 @@
+library(trendecon)
 ### NEW INDICATORS MOMENT #####
 
 ##### CORONA #####
 start <- "2014-01-01"
+geo <- "AT"
 
 corona <- c("Krisenhilfen", "Cofag", "Coronahilfen", "Stundungen", "Kredite", "Inzidenz", "Coronafaelle")
 #corona <- c("Coronahilfen", "Stundungen", "Kredite", "Inzidenz", "Coronafälle")
@@ -19,22 +21,39 @@ proc_index(corona, geo, "corona")
 ##### SKI #####
 ski <- c("Urlaub Tirol", "Skiurlaub Österreich", "Ischgl", "Winterurlaub", "Skifahren Österreich")
 
-proc_keyword_init(ski[1], geo, from = start)
-proc_keyword_init(ski[2], geo, from = start)
-proc_keyword_init(ski[3], geo, from = start)
-proc_keyword_init(ski[4], geo, from = start)
-proc_keyword_init(ski[5], geo, from = start)
-proc_keyword_init(ski[6], geo, from = start)
+proc_keyword_init(ski[1], geo)
+proc_keyword_init(ski[2], geo)
+proc_keyword_init(ski[3], geo)
+proc_keyword_init(ski[4], geo)
+proc_keyword_init(ski[5], geo)
 
 proc_index(ski, geo, "ski")
 
 ##### HANDEL OFFLINE #####
 handel_offline <- c("Shopping Center", "SCS", "SCN", "EKZ", "Geschäft")
 
-proc_keyword_init(handel_offline[1], geo, from = start)
-proc_keyword_init(handel_offline[2], geo, from = start)
-proc_keyword_init(handel_offline[3], geo, from = start)
-proc_keyword_init(handel_offline[4], geo, from = start)
-proc_keyword_init(handel_offline[5], geo, from = start)
+proc_keyword_init(handel_offline[1], geo)
+proc_keyword_init(handel_offline[2], geo)
+proc_keyword_init(handel_offline[3], geo)
+system2("nordvpn","c")
+proc_keyword_init(handel_offline[4], geo)
+proc_keyword_init(handel_offline[5], geo)
 
 proc_index(handel_offline, geo, "handel_offline")
+
+##### GASTRONOMIE OFFLINE #####
+
+gastro <- c("Oeffnungszeiten", "Bar", "Restaurant", "Mittagsmenu", "Speisekarte")
+
+##### ELEKTRO OFFLINE #####
+
+elektro <- c("Geizhals", "Mediamarkt", "e-tec", "willhaben", "Elektronik")
+
+##### GASTRONOMIE OFFLINE #####
+##### GASTRONOMIE OFFLINE #####
+##### GASTRONOMIE OFFLINE #####
+##### GASTRONOMIE OFFLINE #####
+
+
+
+##### GASTRONOMIE OFFLINE #####
